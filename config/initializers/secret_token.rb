6aa7270ccdff4e9c9943994a78be391b8c7e1003
d-rome <dromaryrxfloss@gmail.com>
2011-06-08 22:28:25 -0400

    New rails app with updated .gitignore.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SixthApp::Application.config.secret_token = 'be435d9bbebd91623e776dc436705c562e5abdfefb43b868dbf50b54c234d5d823e529498cec99ee7038c0add74e3174a2078911ee322b30d6baec7f68c5f17c'
